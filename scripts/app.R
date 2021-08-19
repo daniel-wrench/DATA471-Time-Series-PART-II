37a720e474643b0cf97332de9f6990b1ec7fb692
danielpetterson <dpet922@hotmail.com>
2021-08-19 15:09:56 +0930

    interactive plot of generation over time

---
+++ scripts/app.R
@@ -0,0 +1,161 @@
+
+# Load libraries
+library(shiny)
+library(shinydashboard)
+library(RCurl)
+library(tidyverse)
+library(magrittr)
+library(plotly)
+library(imputeTS)
+library(lubridate)
+library(timetk)
+library(viridis)
+
+# Define UI for application that draws a histogram
+ui <- dashboardPage(
+
+    # Application title
+    dashboardHeader(title="Spanish Electricity Generation"),
+
+    # Sidebar with a slider input for number of bins 
+    dashboardSidebar(
+          dateRangeInput("date_select", "Select Date Range",
+                         start = "2015-01-01",
+                         end = "2018-12-31",
+                         min = "2015-01-01",
+                         max = "2018-12-31"),
+            checkboxGroupInput("gen_method_select", "Select Generation Methods",
+                        c("Biomass",
+                          "Lignite",
+                          "Gas",
+                          "Hard Coal",
+                          "Oil",
+                          "Hydro Pumped",
+                          "Hydro River",
+                          "Hydro Reservoir",
+                          "Nuclear",
+                          "Other",
+                          "Other Renewable",
+                          "Solar",
+                          "Waste",
+                          "Wind Onshore"
+                         )),
+            selectInput("vis_select", "Select Visualisation Method",
+                        c("Overlaid",
+                          "Side-by-side"
+                        )),
+          conditionalPanel(
+            "input.vis_select == 'Side-by-side'",
+            selectInput("y_scale_select", "Y Scale",
+                        c("Shared",
+                          "Indepedent"
+                        ))),
+            selectInput("time_period_select", "Smoothing Time Interval",
+                        c("Hour" = "hour",
+                          "Day" = "day",
+                          "Week" = "week",
+                          "Month" = "month",
+                          "Quarter" = "quarter",
+                          "Half Year" = "halfyear",
+                          "Year" = "year"
+                        ), selected = c("Month"="month"))
+        ),
+        dashboardBody(
+          fluidPage(
+            box(plotlyOutput('linePlot'), width = NULL, height = 800)
+          )
+        
+    )
+)
+
+
+server <- function(input, output) {
+  
+    df_long <- reactive({
+      response <- getURL("https://raw.githubusercontent.com/danielpetterson/DATA471-Time-Series/main/Data/energy_dataset.csv")
+      data <- read.csv(text = response, header = TRUE) %>%
+          select(-c(generation.fossil.coal.derived.gas,
+                    generation.fossil.oil.shale,
+                    generation.fossil.peat,
+                    generation.geothermal,
+                    generation.marine,
+                    generation.hydro.pumped.storage.aggregated,
+                    generation.wind.offshore,
+                    forecast.wind.offshore.eday.ahead)) %>% 
+          mutate(across(generation.biomass:price.actual, .fns = na_interpolation, option = 'linear')) %>% 
+          select(c(1, starts_with("generation")))
+        
+        # Giving vars simpler names
+        names(data) <- c("time",
+                             "Biomass",
+                             "Lignite",
+                             "Gas",
+                             "Hard Coal",
+                             "Oil",
+                             "Hydro Pumped",
+                             "Hydro River",
+                             "Hydro Reservoir",
+                             "Nuclear",
+                             "Other",
+                             "Other Renewable",
+                             "Solar",
+                             "Waste",
+                             "Wind Onshore")
+        
+        data$time <- as_datetime(data$time)
+        
+        data %<>%
+          filter(time >= as_datetime(input$date_select[1])) %>%
+          filter(time <= as_datetime(input$date_select[2])) %>%
+          pivot_longer(cols = -time, names_to = "generation_source")
+
+        if(length(input$gen_method_select) > 0) {
+            data %<>%
+                dplyr::filter(generation_source %in% input$gen_method_select)
+        }
+        data
+    })
+    
+
+    output$linePlot <- renderPlotly({
+        req(df_long())
+      
+      # Plotting margins
+      m <- list(
+        l = 50,
+        r = 50,
+        b = 100,
+        t = 100,
+        pad = 4
+      )
+      
+      plot1_data <- df_long() %>% group_by(generation_source) %>% 
+        summarise_by_time(time, .by = input$time_period_select, adjusted = mean(value))
+      
+      plot1 <- ggplot(data=plot1_data) + 
+        geom_line(aes(x = time, y = adjusted, color = generation_source), stat = "identity", lwd = 1, alpha = 0.8) +
+        ggtitle("Power Generation by Method (MW)") +
+        ylab("Power Generated") +
+        xlab("\n\nTime") +
+        scale_color_discrete(name = "Generation Method") +
+        theme(plot.title = element_text(hjust = 0.5),panel.background = element_rect(fill = 'white', color = 'white')) 
+      
+      if (input$vis_select == "Overlaid") {
+        plot1 <- plot1
+     } else if (input$y_scale_select == "Shared") {
+        plot1 <- plot1 +
+         theme(legend.position="none", axis.text.x = element_text(angle = 45)) +
+         facet_wrap(~generation_source)
+     } else {
+        plot1 <- plot1 +
+         theme(legend.position="none", axis.text.x = element_text(angle = 45)) +
+         facet_wrap(~generation_source, scales = "free_y")
+     }
+      ggplotly(plot1) %>%
+        layout(autosize = F, width = 1200, height = 800, margin = m)
+    })
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
